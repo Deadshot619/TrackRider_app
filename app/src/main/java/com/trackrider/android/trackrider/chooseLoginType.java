919747d4c29e2b20663091a99d58d2364a71087c
Deadshot <pankaj.makhija26@gmail.com>
2019-02-19 18:34:28 +0530

    add new login screen and Google sign in button

---
+++ app/src/main/java/com/trackrider/android/trackrider/chooseLoginType.java
@@ -0,0 +1,13 @@
+package com.trackrider.android.trackrider;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class chooseLoginType extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tr_choose_login_type);
+    }
+}
