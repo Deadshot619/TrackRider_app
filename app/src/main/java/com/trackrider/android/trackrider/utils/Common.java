1422f4bf364a78c100c851b1256b19275e419764
Deadshot <pankaj.makhija26@gmail.com>
2019-03-22 14:32:28 +0530

    add new Common class in Utils

---
+++ app/src/main/java/com/trackrider/android/trackrider/utils/Common.java
@@ -0,0 +1,8 @@
+package com.trackrider.android.trackrider.utils;
+
+public class Common {
+    public static final String USER_INFORMATION = "UserInformation";
+    public static final String USER_UID_SAVE_KEY = "SaveUid";
+    public static final String TOKENS = "Tokens";
+    public static User loggedUser;
+}
