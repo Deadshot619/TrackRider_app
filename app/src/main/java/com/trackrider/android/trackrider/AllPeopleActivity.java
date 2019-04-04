8aa97a3ace5d895d738725f845fafac8f832ddcc
Deadshot <pankaj.makhija26@gmail.com>
2019-04-04 22:09:03 +0530

    Add 'AllPeopleActivity' activity & its xml file
    - set Linear Layout
    - make seachview
    - make 'enter email' string

---
+++ app/src/main/java/com/trackrider/android/trackrider/AllPeopleActivity.java
@@ -0,0 +1,13 @@
+package com.trackrider.android.trackrider;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AllPeopleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tr_all_people);
+    }
+}
