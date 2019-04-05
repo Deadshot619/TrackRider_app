e2f4caea704266703c1cf6c67bc9e7535a7f9f15
Deadshot <pankaj.makhija26@gmail.com>
2019-04-05 23:27:23 +0530

    Add FirebaseLoadDone Interface

---
+++ app/src/main/java/com/trackrider/android/trackrider/Interface/IFirebaseLoadDone.java
@@ -0,0 +1,8 @@
+package com.trackrider.android.trackrider.Interface;
+
+import java.util.List;
+
+public interface IFirebaseLoadDone {
+    void onFirebaseLoadUserNameDone(List<String> lstEmail);
+    void onFirebaseLoadFailed(String message);
+}
