01abe7cf356f7e6c8cdbac239340de3105ff546d
Deadshot <pankaj.makhija26@gmail.com>
2019-04-14 12:38:34 +0530

    Add RecyclerView Layout manager

--- app/src/main/java/com/trackrider/android/trackrider/AllPeopleActivity.java
+++ app/src/main/java/com/trackrider/android/trackrider/AllPeopleActivity.java
@@ -2,20 +2,26 @@ package com.trackrider.android.trackrider;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
 import android.text.TextWatcher;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.mancj.materialsearchbar.MaterialSearchBar;
 import com.trackrider.android.trackrider.Interface.IFirebaseLoadDone;
 import com.trackrider.android.trackrider.ViewHolder.UserViewHolder;
+import com.trackrider.android.trackrider.utils.Common;
 import com.trackrider.android.trackrider.utils.User;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class AllPeopleActivity extends AppCompatActivity {
+public class AllPeopleActivity extends AppCompatActivity implements IFirebaseLoadDone {
 
     FirebaseRecyclerAdapter<User, UserViewHolder> adapter, searchAdapter;
     RecyclerView recycler_all_user;
@@ -78,10 +84,35 @@ public class AllPeopleActivity extends AppCompatActivity {
             }
         });
 
+        recycler_all_user = findViewById(R.id.rv_all_people);
+        recycler_all_user.setHasFixedSize(true);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recycler_all_user.setLayoutManager(layoutManager);
+        recycler_all_user.addItemDecoration(new DividerItemDecoration(this, ((LinearLayoutManager) (layoutManager)).getOrientation()));
+
+        firebaseLoadDone = this;
+        loadUserList();
+        //loadSearchData();
 
     }
 
+    private void loadUserList() {
+        Query query = FirebaseDatabase.getInstance().getReference().child(Common.USER_INFORMATION);
+        //FirebaseRecyclerOptions<User> options = new
+    }
+
     private void startSearch(String toString) {
+
+    }
+
+    @Override
+    public void onFirebaseLoadUserNameDone(List<String> lstEmail) {
+
+    }
+
+    @Override
+    public void onFirebaseLoadFailed(String message) {
 
     }
 }
