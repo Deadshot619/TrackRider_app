c267b2240e0d44f047efa4670d387dfde92b6a12
Deadshot <pankaj.makhija26@gmail.com>
2019-04-15 22:40:21 +0530

    Add new layout & java file
    - ChooseLoginType2Activity.java
    - activity_tr_choose_login_type_2.xml

---
+++ app/src/main/java/com/trackrider/android/trackrider/ChooseLoginType2Activity.java
@@ -0,0 +1,160 @@
+package com.trackrider.android.trackrider;
+
+import android.Manifest;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+import com.trackrider.android.trackrider.Interface.IFirebaseLoadDone;
+import com.trackrider.android.trackrider.utils.Common;
+import com.trackrider.android.trackrider.utils.User;
+
+import java.util.Arrays;
+import java.util.List;
+
+import io.paperdb.Paper;
+
+public class ChooseLoginType2Activity extends AppCompatActivity {
+
+    DatabaseReference user_information;
+    private static final int MY_REQUEST_CODE = 7117;
+    List<AuthUI.IdpConfig> providers;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tr_choose_login_type_2);
+
+        Paper.init(this);
+
+        //Init Firebase
+        user_information = FirebaseDatabase.getInstance().getReference(Common.USER_INFORMATION);
+
+        //Init providers
+        providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build()
+        );
+
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+                        showSignInOptions();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+                        Toast.makeText(ChooseLoginType2Activity.this, "You must accept permission to use this application", Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+
+                    }
+                }).check();
+
+    }
+
+    private void showSignInOptions() {
+        startActivityForResult(AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build()
+                ,MY_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == MY_REQUEST_CODE){
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if (resultCode == RESULT_OK){
+                final FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+                //check if user exists on database
+                user_information.orderByKey()
+                        .equalTo(firebaseUser.getUid())
+                        .addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                //if user does not exists
+                                if (dataSnapshot.getValue() == null){
+                                    //if key uid does not exists
+                                    if (!dataSnapshot.child(firebaseUser.getUid()).exists()){
+                                        Common.loggedUser = new User(firebaseUser.getUid(), firebaseUser.getEmail());
+                                        //Add to database
+                                        user_information.child(Common.loggedUser.getUid())
+                                                .setValue(Common.loggedUser);
+                                    }
+                                }else{  //if user is available
+                                    Common.loggedUser = dataSnapshot.child(firebaseUser.getUid()).getValue(User.class);
+                                }
+                                //Save UID to storage to update location from background
+                                Paper.book().write(Common.USER_UID_SAVE_KEY, Common.loggedUser.getUid());
+                                updateToken(firebaseUser);
+                                setupUI();
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                            }
+                        });
+            }
+        }
+
+    }
+
+    private void setupUI() {
+        //Navigate to home
+        startActivity(new Intent(ChooseLoginType2Activity.this, HomePageActivity.class));
+        finish();
+    }
+
+    private void updateToken(final FirebaseUser firebaseUser) {
+        final DatabaseReference tokens = FirebaseDatabase.getInstance()
+                .getReference(Common.TOKENS);
+
+        //Get Token
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnSuccessListener(new OnSuccessListener<InstanceIdResult>() {
+                    @Override
+                    public void onSuccess(InstanceIdResult instanceIdResult) {
+                        tokens.child(firebaseUser.getUid())
+                                .setValue(instanceIdResult.getToken());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(ChooseLoginType2Activity.this, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
+}
+
