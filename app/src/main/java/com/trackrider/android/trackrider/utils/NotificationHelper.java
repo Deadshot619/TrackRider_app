16517f20205ace0240cd4d5f67eef6fcf20ea1f9
Deadshot <pankaj.makhija26@gmail.com>
2019-04-16 18:35:04 +0530

    Add Notification.Builder in NotificationHelper.java

--- app/src/main/java/com/trackrider/android/trackrider/utils/NotificationHelper.java
+++ app/src/main/java/com/trackrider/android/trackrider/utils/NotificationHelper.java
@@ -5,8 +5,11 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.ContextWrapper;
+import android.net.Uri;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
+
+import com.trackrider.android.trackrider.R;
 
 public class NotificationHelper extends ContextWrapper {
     private static final String EDMT_CHANNEL_ID = "com.trackrider.android.trackrider";
@@ -33,9 +36,19 @@ public class NotificationHelper extends ContextWrapper {
         getManager().createNotificationChannel(channel);
     }
 
-    private NotificationManager getManager() {
+    public NotificationManager getManager() {
         if (manager == null)
             manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         return manager;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public Notification.Builder getRealTimeTrackingNotification(String title, String content, Uri defaultSound) {
+        return new Notification.Builder(getApplicationContext(), EDMT_CHANNEL_ID)
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentTitle(title)
+                .setContentText(content)
+                .setSound(defaultSound)
+                .setAutoCancel(false);
     }
 }
