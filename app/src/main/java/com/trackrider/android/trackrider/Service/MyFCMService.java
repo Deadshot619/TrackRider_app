003fe7f7300b7c5b82ee697075848ff86a633e61
Deadshot <pankaj.makhija26@gmail.com>
2019-04-16 18:34:16 +0530

    Complete MyFCMService.java file

--- app/src/main/java/com/trackrider/android/trackrider/Service/MyFCMService.java
+++ app/src/main/java/com/trackrider/android/trackrider/Service/MyFCMService.java
@@ -1,11 +1,89 @@
 package com.trackrider.android.trackrider.Service;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.IBinder;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+import com.trackrider.android.trackrider.R;
+import com.trackrider.android.trackrider.utils.Common;
+import com.trackrider.android.trackrider.utils.NotificationHelper;
+
+import java.util.Map;
+import java.util.Random;
 
 public class MyFCMService extends FirebaseMessagingService {
 
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        if (remoteMessage.getData() != null){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+                sendNotificationWithChannel(remoteMessage);
+            else
+                sendNotification(remoteMessage);
+        }
+
+    }
+
+    private void sendNotification(RemoteMessage remoteMessage) {
+        Map<String, String> data = remoteMessage.getData();
+        String title = "Friend Requests";
+        String content = "New friend requests from " + data.get(Common.FROM_NAME);
+
+        Uri defaultSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentTitle(title)
+                .setContentText(content)
+                .setSound(defaultSound)
+                .setAutoCancel(false);
+
+        NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.notify(new Random().nextInt(), builder.build());
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void sendNotificationWithChannel(RemoteMessage remoteMessage) {
+        Map<String, String> data = remoteMessage.getData();
+        String title = "Friend Requests";
+        String content = "New friend requests from " + data.get(Common.FROM_NAME);
+
+        NotificationHelper helper;
+        Notification.Builder builder;
+
+        Uri defaultSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        helper = new NotificationHelper(this);
+        builder = helper.getRealTimeTrackingNotification(title, content, defaultSound);
+
+        helper.getManager().notify(new Random().nextInt(), builder.build());
+    }
+
+    @Override
+    public void onNewToken(String s) {
+        super.onNewToken(s);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null){
+            final DatabaseReference tokens = FirebaseDatabase.getInstance()
+                    .getReference(Common.TOKENS);
+
+            tokens.child(user.getUid()).setValue(s);
+        }
+    }
 }
