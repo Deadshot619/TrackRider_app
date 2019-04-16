9ae3835d52ef66dba58e2a6c30e8c4a90d65158e
Deadshot <pankaj.makhija26@gmail.com>
2019-04-16 20:52:11 +0530

    Add additional data in Common.java

--- app/src/main/java/com/trackrider/android/trackrider/utils/Common.java
+++ app/src/main/java/com/trackrider/android/trackrider/utils/Common.java
@@ -1,9 +1,22 @@
 package com.trackrider.android.trackrider.utils;
 
+import com.trackrider.android.trackrider.Remote.IFCMService;
+import com.trackrider.android.trackrider.Remote.RetrofitClient;
+
 public class Common {
     public static final String USER_INFORMATION = "UserInformation";
     public static final String USER_UID_SAVE_KEY = "SaveUid";
     public static final String TOKENS = "Tokens";
     public static final String FROM_NAME = "FromName";
+    public static final String ACCEPT_LIST = "acceptList";
+    public static final String FROM_UID = "FromUid";
+    public static final String TO_UID = "ToUid";
+    public static final String TO_NAME = "ToName";
     public static User loggedUser;
+
+    public static IFCMService getIFCMService(){
+        return RetrofitClient.getClient("https://fcm.googleapis.com/")
+                .create(IFCMService.class);
+    }
+
 }
