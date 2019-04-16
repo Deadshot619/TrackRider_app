126c3f57a6f2bd46d08ae0601e0f9be52c260f2d
Deadshot <pankaj.makhija26@gmail.com>
2019-04-16 14:47:35 +0530

    Make MyFCMService.java extend FirebaseMessagingService

--- app/src/main/java/com/trackrider/android/trackrider/Service/MyFCMService.java
+++ app/src/main/java/com/trackrider/android/trackrider/Service/MyFCMService.java
@@ -4,13 +4,8 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
-public class MyFCMService extends Service {
-    public MyFCMService() {
-    }
+import com.google.firebase.messaging.FirebaseMessagingService;
 
-    @Override
-    public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
+public class MyFCMService extends FirebaseMessagingService {
+
 }
