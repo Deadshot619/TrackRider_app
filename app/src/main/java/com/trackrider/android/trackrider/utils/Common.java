32e11c84cce34e6066c8d5f99073a5c7de4f8ef6
Deadshot <pankaj.makhija26@gmail.com>
2019-04-16 15:04:37 +0530

    Add new String FROM_NAME (Common.java)

--- app/src/main/java/com/trackrider/android/trackrider/utils/Common.java
+++ app/src/main/java/com/trackrider/android/trackrider/utils/Common.java
@@ -4,5 +4,6 @@ public class Common {
     public static final String USER_INFORMATION = "UserInformation";
     public static final String USER_UID_SAVE_KEY = "SaveUid";
     public static final String TOKENS = "Tokens";
+    public static final String FROM_NAME = "FromName";
     public static User loggedUser;
 }
