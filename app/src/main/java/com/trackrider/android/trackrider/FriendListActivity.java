22fe76dd98bdabeca8c2bdb4c2f28e633af26970
Deadshot <pankaj.makhija26@gmail.com>
2019-04-18 20:31:04 +0530

    Create new activity FriendListActivity
    - copy paste tr_activity_all_people.xml in tr_activity_friend_list.xml
    - copy paste certain functions from AllPeopleActivity.java

---
+++ app/src/main/java/com/trackrider/android/trackrider/FriendListActivity.java
@@ -0,0 +1,223 @@
+package com.trackrider.android.trackrider;
+
+import android.Manifest;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Typeface;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.mancj.materialsearchbar.MaterialSearchBar;
+import com.trackrider.android.trackrider.Interface.IFirebaseLoadDone;
+import com.trackrider.android.trackrider.Interface.IRecyclerItemClickListener;
+import com.trackrider.android.trackrider.Service.MyLocationReceiver;
+import com.trackrider.android.trackrider.ViewHolder.UserViewHolder;
+import com.trackrider.android.trackrider.utils.Common;
+import com.trackrider.android.trackrider.utils.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FriendListActivity extends AppCompatActivity implements IFirebaseLoadDone {
+
+    FirebaseRecyclerAdapter<User, UserViewHolder> adapter, searchAdapter;
+    RecyclerView recycler_friend_list;
+    IFirebaseLoadDone firebaseLoadDone;
+
+
+    MaterialSearchBar mSearchBar;
+    List<String> mSuggestList = new ArrayList<>();
+
+    DatabaseReference publicLocation;
+    LocationRequest locationRequest;
+    FusedLocationProviderClient fusedLocationProviderClient;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tr_activity_friend_list);
+
+        //Init view
+        mSearchBar = findViewById(R.id.sv_material_search_all_people);
+        //MaterialSearchBar function
+        mSearchBar.setCardViewElevation(10);
+        mSearchBar.addTextChangeListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                List<String> suggest = new ArrayList<>();
+                for (String search : mSuggestList) {
+                    if (search.toLowerCase().contains(mSearchBar.getText().toLowerCase())) {
+                        suggest.add(search);
+                    }
+                }
+                mSearchBar.setLastSuggestions(suggest);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        mSearchBar.setOnSearchActionListener(new MaterialSearchBar.OnSearchActionListener() {
+            @Override
+            public void onSearchStateChanged(boolean enabled) {
+                if (!enabled) {
+                    if (adapter != null) {
+                        //if close search, restore default
+                        recycler_friend_list.setAdapter(adapter);
+                    }
+                }
+            }
+
+            @Override
+            public void onSearchConfirmed(CharSequence text) {
+                startSearch(text.toString());
+            }
+
+            @Override
+            public void onButtonClicked(int buttonCode) {
+
+            }
+        });
+
+        recycler_friend_list = findViewById(R.id.rv_friend_list);
+        recycler_friend_list.setHasFixedSize(true);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recycler_friend_list.setLayoutManager(layoutManager);
+        recycler_friend_list.addItemDecoration(new DividerItemDecoration(this,
+                ((LinearLayoutManager) layoutManager).getOrientation()));
+
+        //Update Location
+        publicLocation = FirebaseDatabase.getInstance().getReference(Common.PUBLIC_LOCATION);
+        updateLocation();
+
+        firebaseLoadDone = this;
+
+        loadFriendList();
+        loadSerchData();
+    }
+
+    private void loadSerchData() {
+    }
+
+    private void loadFriendList() {
+        Query query = FirebaseDatabase.getInstance()
+                .getReference(Common.USER_INFORMATION)
+                .child(Common.loggedUser.getUid())
+                .child(Common.ACCEPT_LIST);
+
+        FirebaseRecyclerOptions<User> options = new FirebaseRecyclerOptions.Builder<User>()
+                .setQuery(query, User.class)
+                .build();
+
+        adapter = new FirebaseRecyclerAdapter<User, UserViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull UserViewHolder holder, int position, @NonNull User model) {
+                holder.mUserEmail.setText(new StringBuilder(model.getEmail_id()));
+
+                holder.setiRecyclerItemClickListener(new IRecyclerItemClickListener() {
+                    @Override
+                    public void onItemClickListener(View view, int position) {
+                        //Show tracking
+                    }
+                });
+            }
+
+            @NonNull
+            @Override
+            public UserViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+                View itemView = LayoutInflater.from(viewGroup.getContext())
+                        .inflate(R.layout.tr_layout_recycler_user, viewGroup, false);
+
+                return new UserViewHolder(itemView);
+            }
+        };
+        adapter.startListening();
+        recycler_friend_list.setAdapter(adapter);
+
+    }
+
+    @Override
+    protected void onStop() {
+        if (adapter != null)
+            adapter.stopListening();
+        if (searchAdapter != null)
+            searchAdapter.stopListening();
+        super.onStop();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (adapter != null)
+            adapter.startListening();
+        if (searchAdapter != null)
+            searchAdapter.startListening();
+
+    }
+
+    private void updateLocation() {
+        buildLocationRequest();
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, getPendingIntent());
+    }
+
+    private PendingIntent getPendingIntent() {
+        Intent intent = new Intent(FriendListActivity.this, MyLocationReceiver.class);
+        intent.setAction(MyLocationReceiver.ACTION);
+        return PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+    private void buildLocationRequest() {
+        locationRequest = new LocationRequest();
+        locationRequest.setSmallestDisplacement(10f);
+        locationRequest.setFastestInterval(3000);
+        locationRequest.setInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+
+    private void startSearch(String toString) {
+    }
+
+    @Override
+    public void onFirebaseLoadUserNameDone(List<String> lstEmail) {
+        mSearchBar.setLastSuggestions(lstEmail);
+    }
+
+    @Override
+    public void onFirebaseLoadFailed(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+}
